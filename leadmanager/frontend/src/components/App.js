767d17d0b61b6988954798cc90a866c9947c06f7
itsaiub <itsaiub@gmail.com>
2019-07-29 13:19:40 +0600

    implement react setup to the project

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./layout/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
